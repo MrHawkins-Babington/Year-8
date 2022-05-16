14ac4928d4284c50a2bf4b8d6208f548ee115bcd
Jason Hawkins <95847296+MrHawkins-Babington@users.noreply.github.com>
2022-05-16 16:07:29 +0100

    Update quizQuestion.py

--- quizQuestion.py
+++ quizQuestion.py
@@ -1,17 +1,13 @@
 #Quiz Question
 
-points = 3
 #Q1
 answer = input("What is the capital of France?")
 answer = answer.title()
 while answer != "Paris":
     answer = input("Incorrect, try again: ")
-    points = points - 1
     answer = answer.title()
 print("Correct! Well done.")
-if points < 0:
-    points = 0
-print("You have",points,"points")
+
 
 #Q2
 answer = input("Where is the Brandenburg Gate?")
@@ -20,8 +16,6 @@ while answer != "Berlin":
     answer = input("Incorrect, try again: ")
     answer = answer.title()
 print("Correct! Well done.")
-points = points + 1
-print("You have",points,"points")
 
 #Q3
 answer = input("Where is the Colliseum?")
@@ -30,5 +24,3 @@ while answer != "Rome":
     answer = input("Incorrect, try again: ")
     answer = answer.title()
 print("Correct! Well done.")
-points = points + 1
-print("You have",points,"points")
